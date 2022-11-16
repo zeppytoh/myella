54b5b8bdf76aaaf435b461447fff2a2e43f402ba
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2022-11-16 22:24:51 +0200

    added: ready

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontSize: {
       xs: [
@@ -83,6 +83,60 @@ module.exports = {
           lineHeight: "1",
         },
       ],
+      "10xl": [
+        "8.5rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "11xl": [
+        "9rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "12xl": [
+        "9.5rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "13xl": [
+        "10rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "14xl": [
+        "11rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "15xl": [
+        "12rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "16xl": [
+        "13rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "17xl": [
+        "14rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "18xl": [
+        "15rem",
+        {
+          lineHeight: "1",
+        },
+      ],
     },
     extend: {
       backgroundImage: (theme) => ({
@@ -90,23 +144,60 @@ module.exports = {
       }),
 
       colors: {
-        "accent": {
-          "50": "#E5E5FF",
-          "100": "#CCCCFF",
-          "200": "#9999FF",
-          "300": "#6666FF",
-          "400": "#3333FF",
-          "500": "#0000FF",
-          "600": "#0000CC",
-          "700": "#000099",
-          "800": "#000066",
-          "900": "#000033"
+        orange: {
+          50: "#FEF0EC",
+          100: "#FDE4DE",
+          200: "#FAC6B7",
+          300: "#F8AB96",
+          400: "#F58D70",
+          500: "#F3734E",
+          600: "#EF4010",
+          700: "#B5310C",
+          800: "#772008",
+          900: "#3E1104",
+        },
+        cyan: {
+          50: "#f7fcfd",
+          100: "#effafb",
+          200: "#d8f1f5",
+          300: "#c1e9ef",
+          400: "#92d9e4",
+          500: "#63c8d8",
+          600: "#59b4c2",
+          700: "#4a96a2",
+          800: "#3b7882",
+          900: "#31626a",
+        },
+        yellow: {
+          50: "#fffcf7",
+          100: "#fff8ef",
+          200: "#ffeed7",
+          300: "#ffe3be",
+          400: "#ffce8e",
+          500: "#ffb95d",
+          600: "#e6a754",
+          700: "#bf8b46",
+          800: "#996f38",
+          900: "#7d5b2e",
+        },
+        red: {
+          50: "#fff8f8",
+          100: "#fff1f1",
+          200: "#ffdcdc",
+          300: "#ffc7c7",
+          400: "#ff9c9c",
+          500: "#ff7272",
+          600: "#e66767",
+          700: "#bf5656",
+          800: "#994444",
+          900: "#7d3838",
         },
       },
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+        mono: ["Space Mono", ...defaultTheme.fontFamily.mono],
       },
     },
   },
   plugins: [],
-}
+};
