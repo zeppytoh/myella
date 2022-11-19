84d69e229cc111c250402b664e742ae484bdeecf
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2022-11-19 16:30:48 +0200

    added: ready

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -213,7 +213,7 @@ module.exports = {
       },
       fontFamily: {
         sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
-        mono: ["Space Mono", ...defaultTheme.fontFamily.mono],
+       
       },
     },
   },
