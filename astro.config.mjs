166b0f529d674465fe11c15c34bfadfd9d2242e0
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2023-01-13 02:48:29 +0200

    ready to sell

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import compress from "astro-compress";
-import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
-   site: 'https://supremeui.dev',
-  integrations: [tailwind(), image(), compress(), partytown(), sitemap()]
+   site: 'https://lexingtonthemes.com',
+  integrations: [tailwind(), image(), compress(),  sitemap()]
 });
